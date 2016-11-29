74b4897938dbe5e4859f477db326607af8a88522
Adrian Brandemuehl <abrandemuehl@gmail.com>
2016-11-29 00:19:40 -0600

    UART driver files

--- Makefile
+++ Makefile
@@ -1,19 +1,17 @@
-app.bin: CC = xtensa-lx106-elf-gcc
-app.bin: CFLAGS = -I. -mlongcalls -nostdlib
-app.bin: CXX = xtensa-lx106-elf-g++
-app.bin: CXXFLAGS = $(CFLAGS) -fno-exceptions
-app.bin: LDLIBS = -nostdlib -Wl,--start-group -lmain -lnet80211 -lwpa -llwip -lpp -lphy -lc -Wl,--end-group -lgcc -lm
-app.bin: LDFLAGS = -Teagle.app.v6.ld
+app: CC = xtensa-lx106-elf-gcc
+app: CFLAGS = -I. -mlongcalls -nostdlib
+app: CXX = xtensa-lx106-elf-g++
+app: CXXFLAGS = $(CFLAGS) -fno-exceptions
+app: LDLIBS = -nostdlib -Wl,--start-group -lmain -lnet80211 -lwpa -llwip -lpp -lphy -lc -Wl,--end-group -lgcc -lm
+app: LDFLAGS = -Teagle.app.v6.ld
 
 test: CC = gcc
 test: LDFLAGS = -lm
 
-MAIN = app
-TEST = test
 EXES = app test
 BINDIR = bin
 
-app.bin: $(MAIN)
+$(BINDIR)/app.bin: app
 	esptool.py elf2image --output $(BINDIR)/$^- $^
 
 flash: $(BINDIR)/app.bin
@@ -22,13 +20,19 @@ flash: $(BINDIR)/app.bin
 	# SDK 2.0.0
 	# esptool.py write_flash 0 $(BINDIR)/app-0x00000.bin 0x10000 $(BINDIR)/app-0x10000.bin
 
-$(TEST): test.c trilateration.o
+test: test.c trilateration.o
 
 trilateration.o: trilateration.c trilateration.h
 
-$(MAIN): app.o
+app: app.o uart.o messages.o ringbuffer.o
 
 app.o: app.c
+
+uart.o: uart.c
+
+ringbuffer.o: ringbuffer.c
+
+messages.o: messages.c
 
 clean:
 	rm -f $(EXES) *.o $(BINDIR)/*
--- app.c
+++ app.c
@@ -3,10 +3,26 @@
 #include "user_config.h"
 #include "user_interface.h"
 #include "network_80211.h"
+#include "mem.h"
+
+#include "uart.h"
+#include "messages.h"
+#include "ringbuffer.h"
+
+#define CHANNEL 8
 
 #define MAC_CMP(a, b) \
   (a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && \
    a[3] == b[3] && a[4] == b[4] && a[5] == b[5])
+
+
+
+
+#define message_procTaskPrio        2
+#define message_procTaskQueueLen    2
+os_event_t message_procTaskQueue[message_procTaskQueueLen];
+static void message_procTask(os_event_t *events);
+
 
 
 struct entry {
@@ -18,7 +34,7 @@ struct entry {
 struct entry entries[100];
 size_t end = 0;
 
-int updates[255];
+RingBuffer updates;
 
 void ICACHE_FLASH_ATTR
 promisc_cb(uint8_t *buf, uint16_t len)
@@ -27,8 +43,7 @@ promisc_cb(uint8_t *buf, uint16_t len)
     return;
   }
   struct sniffer_buf *sniffer = (struct sniffer_buf*)buf;
-  buf += sizeof(struct RxControl);
-  struct framectrl_80211 *fc = (struct framectrl_80211*)buf;
+  struct framectrl_80211 *fc = (struct framectrl_80211*)sniffer->buf;
   if(fc->Type == FRAME_TYPE_DATA) {
     struct data_frame_80211 *data_frame = (struct data_frame_80211*)buf;
     int8_t rssi = sniffer->rx_ctrl.rssi;
@@ -41,11 +56,6 @@ promisc_cb(uint8_t *buf, uint16_t len)
         entries[ptr].time = time;
         if(entries[ptr].rssi != rssi) {
           entries[ptr].rssi = rssi;
-          os_printf("Updating %02x:%02x:%02x:%02x:%02x:%02x -> ",
-              data_frame->sa[0], data_frame->sa[1],
-              data_frame->sa[2], data_frame->sa[3],
-              data_frame->sa[4], data_frame->sa[5]);
-          os_printf("RSSI: %x %d\n", rssi, rssi);
         }
         break;
       }
@@ -56,31 +66,49 @@ promisc_cb(uint8_t *buf, uint16_t len)
       entries[end].rssi = rssi;
       entries[ptr].time = time;
       end++;
-
-      os_printf("Adding %02x:%02x:%02x:%02x:%02x:%02x -> ",
-          data_frame->sa[0], data_frame->sa[1],
-          data_frame->sa[2], data_frame->sa[3],
-          data_frame->sa[4], data_frame->sa[5]);
-      os_printf("RSSI: %x %d\n", rssi, rssi);
     }
+
+    // Put the new position into the ringbuffer as been updated
+    rb_push(&updates, ptr);
   }
 }
 
 
-void loop() {
 
+/* message messages[255]; */
+/* RingBuffer message_q; */
+/* int message_index = 0; */
+
+/* message msg; */
+/* uint8_t *msg_next = &msg; */
+/* uint8_t *msg_end = (&msg)+1; */
+/* int msg_done = 0; */
+
+
+// Send updates over uart
+void ICACHE_FLASH_ATTR
+message_procTask(os_event_t *event) {
+  os_printf("Running loop\n");
+  message m;
+  int idx = rb_get(&updates);
+  while(idx != -1) {
+    message_create(&m, system_get_chip_id(), entries[idx].mac, entries[idx].rssi);
+    // uart0_tx_buffer((uint8_t *)&m, sizeof(m));
+  }
+  os_delay_us(1000);
+  system_os_post(message_procTaskPrio, 0, 0);
 }
 
 void ICACHE_FLASH_ATTR
 sniffer_init(void)
 {
   // Set up promiscuous callback
-  wifi_set_channel(1);
+  wifi_set_channel(CHANNEL);
   wifi_promiscuous_enable(0);
   wifi_set_promiscuous_rx_cb(promisc_cb);
   wifi_promiscuous_enable(1);
-  os_printf("Done initializing\n");
   os_memset(entries, 0, sizeof(entries));
+  rb_init(&updates);
 }
 
 
@@ -88,11 +116,18 @@ sniffer_init(void)
 user_init()
 {
   uart_div_modify(0, UART_CLK_FREQ / 115200);
+  /* uart_init(BIT_RATE_115200, BIT_RATE_115200); */
 
   // Promiscuous works only with station mode
   wifi_set_opmode(STATION_MODE);
 
   system_init_done_cb(sniffer_init);
 
-  loop();
+  // rb_init(&messages);
+
+  uint8_t comm_start[100] = {0xFF};
+  comm_start[99] = 0;
+
+  system_os_task(message_procTask, message_procTaskPrio, message_procTaskQueue, message_procTaskQueueLen);
+  system_os_post(message_procTaskPrio, 0, 0);
 }
